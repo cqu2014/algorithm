ebd1f434a20e15986c668807e050e4ac5a7a5212
oliver.wang@shein.com <286931986@qq.com>
2020-11-09 14:53:51 +0800

    add ignore

---
+++ src/main/java/com/oliver/algorithm/pattern/observer/NumberModel.java
@@ -0,0 +1,16 @@
+package com.oliver.algorithm.pattern.observer;
+
+import lombok.Data;
+
+/**
+ * @author Oliver Wang
+ * @description 取号排队中号码的信息
+ * @created by IntelliJ IDEA 2020.02
+ * @date Create at 2020/11/9
+ * @since 
+ */
+@Data
+public class NumberModel {
+    private int number;
+    private String deskType;
+}
